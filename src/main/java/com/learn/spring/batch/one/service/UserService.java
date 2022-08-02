a9bcc4dad558fa18bdd2129fbfe5c659bcc48b6a
Manjunath K B <manju.k.bharath@gmail.com>
2022-08-02 10:30:17 -0400

    new commit

--- src/main/java/com/learn/spring/batch/one/service/UserService.java
+++ src/main/java/com/learn/spring/batch/one/service/UserService.java
@@ -7,6 +7,8 @@ import com.learn.spring.batch.one.entity.UserData;
 import com.learn.spring.batch.one.exceptions.InvalidUserIDException;
 import com.learn.spring.batch.one.exceptions.UserNotFoundException;
 
+/* Interface for User Service
+ * */
 public interface UserService {
 	public List<UserData> getAllUsers();
 
